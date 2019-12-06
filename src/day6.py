d67b24c27540ab45978165da7a9fb0557c43436f
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2019-12-06 06:46:53 +0100

    Optimize day 6

--- src/day6.py
+++ src/day6.py
@@ -4,7 +4,7 @@
 import networkx as nx
 
 from aocd.models import Puzzle
-from funcy import print_calls, takewhile, ilen
+from funcy import print_calls
 
 
 @print_calls
@@ -17,13 +17,7 @@ def part1(graph):
 
 @print_calls
 def part2(graph):
-    you = nx.shortest_path(graph, "COM", "YOU")
-    san = nx.shortest_path(graph, "COM", "SAN")
-
-    # number of nodes that both paths have in common
-    ncommon = ilen(takewhile(lambda tup: tup[0] == tup[1], zip(you, san)))
-    # sum of length from YOU & SAN to common node (-2 for excluding YOU and SAN)
-    return len(you) - ncommon + len(san) - ncommon - 2
+    return nx.shortest_path_length(graph.to_undirected(), "YOU", "SAN") - 2
 
 
 def load(data):
